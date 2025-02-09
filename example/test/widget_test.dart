88c9b0a626594cf4c5fd870cbfeff7c200ac7681
Daniel Herrera sanchez <danielherrerasanchez@MacBook-Pro-de-Daniel.local>
2025-02-08 20:46:42 -0500

    ci ⚙️: Se actualiza y se poner a realizar los test justo al final de comprobar la librería

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,30 +1,35 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Show WcAlert test', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      // Arrange
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Act
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Assert
+      expect(find.text('This is a title'), findsOneWidget);
+      expect(find.text('This is a message'), findsOneWidget);
+    });
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Show WcModal test', (WidgetTester tester) async {
+    await tester.runAsync(() async {
+      // Arrange
+      await tester.pumpWidget(const MyApp());
+
+      // Act
+      await tester.tap(find.byIcon(Icons.remove));
+      await tester.pump();
+
+      // Assert
+      expect(find.text('This is a title'), findsOneWidget);
+      expect(find.text('This is a message'), findsOneWidget);
+    });
   });
 }
